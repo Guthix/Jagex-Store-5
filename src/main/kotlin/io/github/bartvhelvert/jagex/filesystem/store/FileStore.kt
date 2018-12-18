d18a93a1f266bd8f69654fbf777b9359f7873faa
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2018-12-18 11:18:35 +0100

    Add support for reading the filesystem format

---
+++ src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/store/FileStore.kt
@@ -0,0 +1,43 @@
+package io.github.bartvhelvert.jagex.filesystem.store
+
+import java.io.File
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+
+class FileStore(directory: File) {
+    private val dataChannel: DataChannel
+
+    private val indexChannels = mutableMapOf<Int, IndexChannel>()
+
+    private val metaDataChannel: IndexChannel
+
+    init {
+        require(directory.isDirectory)
+        val dataFile = directory.resolve("$FILE_NAME.$DATA_FILE_EXTENSION")
+        require(dataFile.isFile)
+        dataChannel = DataChannel(RandomAccessFile(dataFile, accessMode).channel)
+        for (indexFileId in 0 until META_DATA_INDEX) {
+            val indexFile = directory.resolve("$FILE_NAME$indexFileId.$INDEX_FILE_EXTENSION")
+            if(!indexFile.isFile) continue
+            indexChannels[indexFileId] = IndexChannel(RandomAccessFile(dataFile, accessMode).channel)
+        }
+        val metaDataFile = directory.resolve("$FILE_NAME$META_DATA_INDEX.$INDEX_FILE_EXTENSION")
+        require(metaDataFile.isFile)
+        metaDataChannel = IndexChannel(RandomAccessFile(dataFile, accessMode).channel)
+    }
+
+    @ExperimentalUnsignedTypes
+    fun read(indexFileId: Int, archiveId: Int): ByteBuffer {
+        val index = indexChannels[indexFileId]?.read(archiveId)
+        require(index != null)
+        return dataChannel.read(indexFileId, index, archiveId)
+    }
+
+    companion object {
+        private const val accessMode = "rw"
+        private const val DATA_FILE_EXTENSION = "dat2"
+        private const val INDEX_FILE_EXTENSION = "idx"
+        private const val FILE_NAME = "main_file_cache"
+        private const val META_DATA_INDEX = 255
+    }
+}
