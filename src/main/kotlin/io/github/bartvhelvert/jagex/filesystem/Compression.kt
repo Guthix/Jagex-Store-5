d760dfdd694843a23cc887d733401fcb05e37d0e
bartvhelvert <b.a.j.v.helvert@student.tue.nl>
2018-12-22 20:30:36 +0100

    Fix compression algorithms

--- src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Compression.kt
+++ src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Compression.kt
@@ -4,99 +4,69 @@ import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.SequenceInputStream
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
 enum class Compression(val opcode: Int, val headerSize: Int) {
     NONE(opcode = 0, headerSize = 0) {
         override fun compress(input: ByteArray) = input
-        override fun decompress(input: ByteArray) = input
+        override fun decompress(input: ByteArray, decompressedSize: Int) = input
     },
 
 
     BZIP2(opcode = 1, headerSize = Int.SIZE_BYTES) {
         val BLOCK_SIZE = 1
 
-        val HEADER_SIZE = 2
+        val HEADER = "BZh$BLOCK_SIZE".toByteArray(StandardCharsets.US_ASCII)
 
         override fun compress(input: ByteArray): ByteArray {
             ByteArrayInputStream(input).use { inStream ->
                 val bout = ByteArrayOutputStream()
                 BZip2CompressorOutputStream(bout, BLOCK_SIZE).use { outStream ->
-                    val buf = ByteArray(BUFFER_SIZE)
-                    var len = inStream.read(buf, 0, buf.size)
-                    while (len != -1) {
-                        outStream.write(buf, 0, len)
-                        len = inStream.read(buf, 0, buf.size)
-                    }
-                }
-                val compressed = bout.toByteArray()
-                return compressed.sliceArray(HEADER_SIZE..compressed.size)
-            }
-        }
-
-        override fun decompress(input: ByteArray): ByteArray {
-            val bzip2 = ByteArray(input.size + HEADER_SIZE)
-            bzip2[0] = 'h'.toByte()
-            bzip2[1] = '1'.toByte()
-            System.arraycopy(input, 0, bzip2, HEADER_SIZE, input.size)
-            BZip2CompressorInputStream(ByteArrayInputStream(bzip2)).use { inStream ->
-                val outputStream = ByteArrayOutputStream()
-                outputStream.use { outStream ->
-                    val buf = ByteArray(BUFFER_SIZE)
-                    var len = inStream.read(buf, 0, buf.size)
-                    while (len != -1) {
-                        outStream.write(buf, 0, len)
-                        len = inStream.read(buf, 0, buf.size)
-                    }
-                }
-                return outputStream.toByteArray()
-            }
-        }
-    },
-
-    GZIP(opcode = 2, headerSize = Int.SIZE_BYTES) {
-        override fun compress(input: ByteArray): ByteArray {
-            val inputStream = ByteArrayInputStream(input)
-            inputStream.use { inStream ->
-                val bout = ByteArrayOutputStream()
-                val outputStream = GZIPOutputStream(bout)
-                outputStream.use { outStream ->
-                    val buf = ByteArray(BUFFER_SIZE)
-                    var len = inStream.read(buf, 0, buf.size)
-                    while (len != -1) {
-                        outStream.write(buf, 0, len)
-                        len = inStream.read(buf, 0, buf.size)
-                    }
+                    inStream.transferTo(outStream)
                 }
                 return bout.toByteArray()
             }
         }
 
-        override fun decompress(input: ByteArray): ByteArray {
-            val inputStream = GZIPInputStream(ByteArrayInputStream(input))
-            inputStream.use { inStream ->
-                val outputStream = ByteArrayOutputStream()
-                outputStream.use { outStream ->
-                    val buf = ByteArray(BUFFER_SIZE)
-                    var len = inStream.read(buf, 0, buf.size)
-                    while (len!= -1) {
-                        outStream.write(buf, 0, len)
-                        len = inStream.read(buf, 0, buf.size)
-                    }
-                }
-                return outputStream.toByteArray()
+        override fun decompress(input: ByteArray, decompressedSize: Int): ByteArray {
+            val decompressed = ByteArray(decompressedSize)
+            val str = SequenceInputStream(ByteArrayInputStream(HEADER), ByteArrayInputStream(input))
+            BZip2CompressorInputStream(str).use { inStream ->
+                inStream.readNBytes(decompressed, 0, decompressed.size)
             }
+            return decompressed
+        }
+    },
+
+    GZIP(opcode = 2, headerSize = Int.SIZE_BYTES) {
+        override fun compress(input: ByteArray): ByteArray {
+            ByteArrayInputStream(input).use { inStream ->
+                val bout = ByteArrayOutputStream()
+                GZIPOutputStream(bout).use { outStream ->
+                    inStream.transferTo(outStream)
+                }
+                return bout.toByteArray()
+            }
+        }
+
+        override fun decompress(input: ByteArray, decompressedSize: Int): ByteArray {
+            val decompressed = ByteArray(decompressedSize)
+            GZIPInputStream(ByteArrayInputStream(input)).use { inStream ->
+                inStream.readNBytes(decompressed, 0, decompressed.size)
+            }
+            return decompressed
         }
     };
 
     abstract fun compress(input: ByteArray): ByteArray
 
-    abstract fun decompress(input: ByteArray): ByteArray
+    abstract fun decompress(input: ByteArray, decompressedSize: Int): ByteArray
 
     companion object {
-        const val BUFFER_SIZE = 4096
-
         fun getByOpcode(opcode: Int) = Compression.values().first{ opcode == it.opcode }
     }
 }
