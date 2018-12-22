1ca3d5ce40bcedd3cd485006b65465f4d80f008d
bartvhelvert <b.a.j.v.helvert@student.tue.nl>
2018-12-22 13:16:39 +0100

    Add support for XTEA decryption in cache

--- src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Cache.kt
+++ src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Cache.kt
@@ -4,7 +4,7 @@ import java.io.File
 import io.github.bartvhelvert.jagex.filesystem.store.FileStore
 import java.io.IOException
 
-class Cache(directory: File) {
+class Cache(directory: File, xteas: MutableMap<Int, IntArray> = mutableMapOf()) {
     private val fileStore = FileStore(directory)
 
     private val indexAttributesCache = mutableMapOf<Int, IndexAttributes>()
@@ -12,9 +12,14 @@ class Cache(directory: File) {
     private val dictionaryCache = mutableMapOf<Int, MutableMap<Int, Dictionary>>()
 
     @ExperimentalUnsignedTypes
-    fun readDictionary(indexFileId: Int, dictionaryId: Int, shouldCache: Boolean = false): Dictionary {
+    fun readDictionary(
+        indexFileId: Int,
+        dictionaryId: Int,
+        xteaKey: IntArray = XTEA.ZERO_KEY,
+        shouldCache: Boolean = false
+    ): Dictionary {
         val indexAttributes = readIndexAttributes(indexFileId)
-        val dataContainer: Container = Container.decode(fileStore.read(indexFileId, dictionaryId))
+        val dataContainer: Container = Container.decode(fileStore.read(indexFileId, dictionaryId), xteaKey)
         val dictionaryAttributes = indexAttributes.dictionaryAttributes[indexFileId]
             ?: throw IOException("Dictionary attributes to not exist in the shouldCache")
         val dictionary = Dictionary.decode(dataContainer, dictionaryAttributes)
