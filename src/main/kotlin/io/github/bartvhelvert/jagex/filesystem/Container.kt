8fa461911afa8f3177caa247c4defa0715dde2e7
bartvhelvert <b.a.j.v.helvert@student.tue.nl>
2018-12-22 20:30:56 +0100

    Fix version reading and compression

--- src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Container.kt
+++ src/main/kotlin/io/github/bartvhelvert/jagex/filesystem/Container.kt
@@ -5,28 +5,35 @@ import java.nio.ByteBuffer
 
 internal data class Container(val version: Int, val data: ByteBuffer) {
     companion object {
+        const val ENC_HEADER_SIZE = Byte.SIZE_BYTES + Int.SIZE_BYTES
+
+        const val COMP_HEADER_SIZE = ENC_HEADER_SIZE + Int.SIZE_BYTES
+
         @ExperimentalUnsignedTypes
         internal fun decode(buffer: ByteBuffer, xteaKey: IntArray = XTEA.ZERO_KEY): Container {
             require(xteaKey.size == XTEA.KEY_SIZE)
             val compression = Compression.getByOpcode(buffer.uByte.toInt())
             val compressedSize = buffer.int
-            val compressionBuffer = buffer.slice().decipher(compression, compressedSize, xteaKey)
-            val dataBuffer = compressionBuffer.decompress(compression, compressedSize)
-            val version = compressionBuffer.decodeVersion()
+            buffer.decrypt(xteaKey, compression, compressedSize)
+            val dataBuffer = buffer.decompress(compression, compressedSize)
+            buffer.position(COMP_HEADER_SIZE + compressedSize)
+            val version = buffer.decodeVersion()
             return Container(version, dataBuffer)
         }
 
-        private fun ByteBuffer.decipher(compression: Compression, compressedSize: Int, xteaKey: IntArray) =
-            if(xteaKey.all { it != 0 }) {
+        private fun ByteBuffer.decrypt(xteaKeys: IntArray, compression: Compression, compressedSize: Int) =
+            if (xteaKeys.all { it == 0 }) {
                 this
             } else {
-                xteaDecrypt(xteaKey, end = compression.headerSize + compressedSize)
+                xteaDecrypt(xteaKeys, ENC_HEADER_SIZE, ENC_HEADER_SIZE + compression.headerSize + compressedSize)
             }
 
         private fun ByteBuffer.decompress(compression: Compression, compressedSize: Int): ByteBuffer {
             if(compression == Compression.NONE) return this
             val uncompressedSize = int
-            val uncompressed = compression.decompress(array().sliceArray(0..compressedSize))
+            val uncompressed = compression.decompress(array().sliceArray(
+                COMP_HEADER_SIZE until COMP_HEADER_SIZE+ compressedSize), uncompressedSize
+            )
             if (uncompressed.size != uncompressedSize) throw IOException("Compression size mismatch")
             return ByteBuffer.wrap(uncompressed)
         }
