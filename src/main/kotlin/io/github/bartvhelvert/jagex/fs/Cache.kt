0eae0c35746bd9b2975a6fcdeb796be862f5544e
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-01-09 16:54:15 +0100

    Add full dictionary loading to API

--- src/main/kotlin/io/github/bartvhelvert/jagex/fs/Cache.kt
+++ src/main/kotlin/io/github/bartvhelvert/jagex/fs/Cache.kt
@@ -9,10 +9,23 @@ class Cache(directory: File) {
     private val fileStore = FileStore(directory)
 
     @ExperimentalUnsignedTypes
-    private val dictionaryAttributesCache = Array(fileStore.indexFileCount) {
+    private val dictionaryAttributes = Array(fileStore.indexFileCount) {
         DictionaryAttributes.decode(
             Container.decode(fileStore.read(FileStore.ATTRIBUTE_INDEX, it))
         )
+    }
+
+    @ExperimentalUnsignedTypes
+    fun readArchives(dictionaryId: Int, xteas: Map<Int, IntArray> = emptyMap()): Map<Int, Archive> {
+        if(dictionaryId !in 0..dictionaryAttributes.size) throw IOException("Dictionary does not exist")
+        val dictAttributes = dictionaryAttributes[dictionaryId]
+        val archives = mutableMapOf<Int, Archive>()
+        for((id, archiveAttr) in dictAttributes.archiveAttributes) {
+            val xtea = xteas[id] ?: XTEA.ZERO_KEY
+            val archiveContainer = Container.decode(fileStore.read(dictionaryId, id), xtea)
+            archives[id] = Archive.decode(archiveContainer, archiveAttr)
+        }
+        return archives
     }
 
     @ExperimentalUnsignedTypes
@@ -21,9 +34,9 @@ class Cache(directory: File) {
         archiveId: Int,
         xteaKey: IntArray = XTEA.ZERO_KEY
     ): Archive {
-        if(dictionaryId !in 0..dictionaryAttributesCache.size) throw IOException("Dictionary does not exist")
-        val dictionaryAttributes = dictionaryAttributesCache[dictionaryId]
-        val archiveAttributes = dictionaryAttributes.archiveAttributes[archiveId]
+        if(dictionaryId !in 0..dictionaryAttributes.size) throw IOException("Dictionary does not exist")
+        val dictAttributes = dictionaryAttributes[dictionaryId]
+        val archiveAttributes = dictAttributes.archiveAttributes[archiveId]
             ?: throw IOException("Archive does not exist")
         val archiveContainer = Container.decode(fileStore.read(dictionaryId, archiveId), xteaKey)
         return Archive.decode(archiveContainer, archiveAttributes)
