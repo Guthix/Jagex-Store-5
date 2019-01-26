a793cb810fd4b206afc5114581af53786d7ef3b2
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-01-26 02:22:50 +0100

    Add a license notice as file header

--- src/main/kotlin/io/github/bartvhelvert/jagex/fs/io/Utility.kt
+++ src/main/kotlin/io/github/bartvhelvert/jagex/fs/io/Utility.kt
@@ -1,5 +1,23 @@
-package io.github.bartvhelvert.jagex.fs.io
+/*
+GNU LGPL V3
+Copyright (C) 2019 Bart van Helvert
+B.A.J.v.Helvert@gmail.com
 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package io.github.bartvhelvert.jagex.fs.io
 
 val charset = charArrayOf('€', '\u0000', '‚', 'ƒ', '„', '…', '†', '‡', 'ˆ', '‰', 'Š', '‹', 'Œ', '\u0000',
     'Ž', '\u0000', '\u0000', '‘', '’', '“', '”', '•', '–', '—', '˜', '™', 'š', '›', 'œ', '\u0000', 'ž', 'Ÿ'
