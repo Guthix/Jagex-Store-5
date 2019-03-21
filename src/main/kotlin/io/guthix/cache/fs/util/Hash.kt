2fad4a23dc2f0e8d14c0f333e260b9e93ca5902d
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-03-21 22:48:55 +0100

    Change name scheme

--- src/main/kotlin/io/guthix/cache/fs/util/Hash.kt
+++ src/main/kotlin/io/guthix/cache/fs/util/Hash.kt
@@ -23,7 +23,7 @@ import java.security.Security
 
 const val whirlPoolHashByteCount = 64
 
-fun whirlPoolHash(data: ByteArray):ByteArray {
+fun whirlPoolHash(data: ByteArray): ByteArray {
     Security.addProvider(BouncyCastleProvider())
     return MessageDigest.getInstance("Whirlpool").digest(data)
 }
