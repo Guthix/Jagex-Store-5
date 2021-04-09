c6421deadc17e965989b5a81ca65b98e8e2d1456
Matthew <matthew@mahabal.tech>
2021-04-09 09:08:23 -0500

    Update revision to latest

--- toolbox/downloader/build.gradle.kts
+++ toolbox/downloader/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { application }
 
 val homePath: String = System.getProperty("user.home")
 
-val revision: Int = 190 // change this to latest revision before running
+val revision: Int = 194 // change this to latest revision before running
 val includeVersions: Boolean = true // whether to append versions to every group
 
 val logbackVersion: String by rootProject.extra
